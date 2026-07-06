64e6f8d6dc04bca3a8d6428893443e9516674c8c
Walter McKelvie <walt@mckelvie.org>
2026-07-06 01:20:59 -0400

    update wayle-notifications patch

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
         #       "wayle-core-0.1.2"    = lib.fakeHash;  # rev c0ccb44…
         #     };
         #   };
-        cargoHash = "sha256-P+Xne5+mNaDrpdelxjuxob3tXVvlbkyOewGfZEwz9xU=";
+        cargoHash = "sha256-6jD2qMgli9bkNBHqahekBCyrXWoJs/qS5ea4lHdkHCk=";
 
         nativeBuildInputs = with pkgs; [
           pkg-config
